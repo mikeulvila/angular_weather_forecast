308b4bf5e83cb14ecbad084bacd37e502c2df30b
Mike Ulvila <mikeulvila@me.com>
2016-06-13 13:52:49 -0500

    create weatherService

--- services.js
+++ services.js
@@ -4,3 +4,24 @@ weatherApp.service('cityService', function () {
   this.city = "New York, NY";
 
 });
+
+weatherApp.service('weatherService', ['$resource', function ($resource) {
+
+    var APIKey = '';
+
+    this.GetWeather = function (city, days) {
+
+      var weatherAPI =
+      $resource('http://api.openweathermap.org/data/2.5/forecast?appid=' + APIKey,
+          { callback: 'JSON_CALLBACK' }, {
+            get: {
+              method: 'JSONP'
+            }
+          }
+      );
+
+      return weatherAPI.get({ q: city, cnt: days });
+
+    };
+
+}]);
