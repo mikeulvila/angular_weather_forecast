14e41149d3a205136bf6ca946a6f945049b6aa78
Mike Ulvila <mikeulvila@me.com>
2016-06-13 13:12:17 -0500

    refactored to form ng-submit

--- controllers.js
+++ controllers.js
@@ -1,12 +1,16 @@
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', 'cityService',
-  function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService',
+  function($scope, $location, cityService) {
 
   $scope.city = cityService.city;
 
   $scope.$watch('city', function () {
     cityService.city = $scope.city;
   });
+
+  $scope.submit = function () {
+     $location.path('/forecast');
+  };
 
 }]);
 
